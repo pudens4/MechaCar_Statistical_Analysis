3dda83fe0afb179169577642dd350a2e41bf76d6
Pudens Anilus <pudensanilus@pudenss-imac.home>
2021-11-28 16:06:58 -0500

    t test

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,19 @@ data.frame <- read.csv(file = '/Volumes/Pud SSD/Analytics/Columbia University Bo
 #perform linear regression
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = data.frame)
 summary(reg)
+
+
+data.coil <- read.csv(file = '/Volumes/Pud SSD/Analytics/Columbia University Bootcamp/Module15/MechaCar_Statistical_Analysis/Suspension_Coil.csv', header = TRUE)
+summarise(data.coil, mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+
+
+group_manuf <- group_by(data.coil, Manufacturing_Lot )
+summarise(group_manuf, mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+
+
+# t test
+t.test(data.coil$PSI, mu=1500)
+
+t.test(subset(data.coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(data.coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(data.coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
